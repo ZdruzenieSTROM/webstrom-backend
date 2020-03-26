1f2302fe3faf7f01ad9ad5596bed4ebe1b37c1e9
michal <m.masrna1@gmail.com>
2020-03-26 15:12:52 +0100

    #37 zmena hesla

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,9 @@ from django.contrib.auth.views import (LoginView, LogoutView,
                                        PasswordResetCompleteView,
                                        PasswordResetConfirmView,
                                        PasswordResetDoneView,
-                                       PasswordResetView)
+                                       PasswordResetView,
+                                       PasswordChangeView,
+                                       PasswordChangeDoneView)
 from django.urls import path, reverse_lazy
 
 from user.views import (SignupView, VerificationSendView,
@@ -43,4 +45,15 @@ urlpatterns = [
              template_name='user/password_reset_complete.html'
          ),
          name='password_reset_complete'),
+    path('password-change/',
+         PasswordChangeView.as_view(
+             template_name='user/password_change.html',
+             success_url=reverse_lazy('user:password_change_done'),
+         ),
+         name='password_change'),
+    path('password-change/done/',
+         PasswordChangeDoneView.as_view(
+             template_name='user/password_change_done.html'
+         ),
+         name='password_change_done'),
 ]
