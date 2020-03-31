328b4b0c9b0ae8c68a1c5ff697aecc01dc4d52e1
Martin Mihálik <34107535+mmihalik@users.noreply.github.com>
2020-03-31 23:19:32 +0200

    Rozdelenie user modelu

--- user/admin.py
+++ user/admin.py
@@ -1,53 +1,71 @@
+from django import forms
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.models import Group, Permission
 
-from .forms import CustomUserChangeForm, CustomUserCreationForm
-from .models import County, District, School, User
+from .models import County, District, School, User, Profile
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Heslo', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Potvrdenie hesla', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Heslá sa nezhodujú')
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password',)
+
+    password = ReadOnlyPasswordHashField()
+
+    def clean_password(self):
+        return self.initial['password']
 
 
 @admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = User
-    list_per_page = 100
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
 
-    list_display = (
-        'email',
-        'first_name',
-        'last_name',
-        'is_staff'
-    )
-    list_filter = (
-        'is_active',
-        'is_staff',
-        'is_superuser',
-        'groups'
-    )
-
-    search_fields = [
-        'first_name',
-        'last_name',
-        'email',
-        'school',
-    ]
-    ordering = ('last_name', 'first_name')
-
-    # filter_horizontal = ['groups', 'user_permissions']
+    list_display = ('email', 'is_staff',)
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'groups',)
+    search_fields = ('email',)
+    ordering = ('email',)
     readonly_fields = ('last_login',)
+    inlines = (ProfileInline,)
 
     fieldsets = (
         ('Prihlásenie', {
             'classes': ('wide',),
-            'fields': ('email', 'email_verified', 'password', 'last_login'),
-        }),
-        ('Osoba', {
-            'classes': ('wide',),
-            'fields': ('first_name', 'last_name', 'school', ('phone', 'parent_phone')),
+            'fields': ('email', 'verified_email', 'password', 'last_login'),
         }),
         ('Oprávnenia', {
-            'classes': ('wide',),
+            'classes': ('collapse',),
             'fields': (('is_staff', 'is_superuser'), 'groups'),
         }),
         ('Extra oprávnenia', {
@@ -60,17 +78,9 @@ class CustomUserAdmin(UserAdmin):
     )
 
     add_fieldsets = (
-        ('Prihlásenie', {
+        (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password', 'password2'),
-        }),
-        ('Osoba', {
-            'classes': ('wide',),
-            'fields': ('first_name', 'last_name', 'school', ('phone', 'parent_phone')),
-        }),
-        ('Oprávnenia', {
-            'classes': ('wide',),
-            'fields': (('is_staff', 'is_superuser'), 'groups'),
+            'fields': ('email', 'password1', 'password2'),
         }),
     )
 
--- user/models.py
+++ user/models.py
@@ -5,103 +5,70 @@ from django.db import models
 from base.validators import phone_number_validator
 
 
-class County(models.Model):
-    class Meta:
-        verbose_name = 'kraj'
-        verbose_name_plural = 'kraje'
-
-    code = models.AutoField(primary_key=True, verbose_name='kód')
-    name = models.CharField(max_length=30, verbose_name='názov')
-
-    def __str__(self):
-        return self.name
-
-
-class District(models.Model):
-    class Meta:
-        verbose_name = 'okres'
-        verbose_name_plural = 'okresy'
-
-    code = models.AutoField(primary_key=True, verbose_name='kód')
-    name = models.CharField(max_length=30, verbose_name='názov')
-    county = models.ForeignKey(County, on_delete=models.CASCADE)
-    abbreviation = models.CharField(max_length=2, verbose_name='skratka')
-
-    def __str__(self):
-        return self.name
-
-
-class School(models.Model):
-    class Meta:
-        verbose_name = 'škola'
-        verbose_name_plural = 'školy'
-
-    code = models.AutoField(primary_key=True, verbose_name='kód')
-    name = models.CharField(max_length=100, verbose_name='názov')
-    abbreviation = models.CharField(max_length=10, verbose_name='skratka')
-    street = models.CharField(max_length=100, verbose_name='ulica')
-    city = models.CharField(max_length=100, verbose_name='obec')
-    zip_code = models.CharField(max_length=6, verbose_name='PSČ')
-    email = models.CharField(max_length=50, verbose_name='email', null=True)
-    district = models.ForeignKey(District, on_delete=models.CASCADE)
-
-    def __str__(self):
-        # TODO: Nejaky pekny vypis skoly
-        # TODO: dočasne
-        return f'{ self.name }, { self.street }, { self.city }'
-
-    def stitok(self):
-        return f'\stitok{{{ self.nazov }}}{{{ self.city }}}{{{ self.zip }}}{{{ self.street }}}'
-
-
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('Používateľ musí mať nastavený email.')
+            raise ValueError('Email musí byť nastavený')
 
-        user = self.model(
-            email=self.normalize_email(email),
-            **extra_fields
-        )
-
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save()
-
+        user.save(using=self._db)
         return user
 
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(email, password, **extra_fields)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser musí byť správca')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser musí mať is_superuser=True')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    class Meta(AbstractBaseUser.Meta):
+        verbose_name = 'používateľ'
+        verbose_name_plural = 'používatelia'
+
     email = models.EmailField(
-        unique=True,
-        verbose_name='e-mail',
-    )
-    email_verified = models.BooleanField(
-        default=False,
-        verbose_name='overený e-mail'
-    )
+        unique=True, max_length=255, verbose_name='email')
+    verified_email = models.BooleanField(
+        default=False, verbose_name='overený email')
 
-    first_name = models.CharField(
-        max_length=32,
-        verbose_name='krstné meno'
-    )
-    last_name = models.CharField(
-        max_length=32,
-        verbose_name='priezvisko'
-    )
+    is_staff = models.BooleanField(
+        default=False, verbose_name='správcovský prístup')
+    is_active = models.BooleanField(default=True, verbose_name='je aktívny')
 
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class Profile(models.Model):
+    class Meta:
+        verbose_name = 'profil'
+        verbose_name_plural = 'profily'
+
+    user = models.OneToOneField('user.User', on_delete=models.CASCADE)
+
+    first_name = models.CharField(max_length=32, verbose_name='krstné meno')
+    last_name = models.CharField(max_length=32, verbose_name='priezvisko')
     nickname = models.CharField(
-        max_length=32,
-        verbose_name='prezývka'
-    )
+        max_length=32, blank=True, null=True, verbose_name='prezývka')
 
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    school = models.ForeignKey('user.School', on_delete=models.CASCADE)
 
     year_of_graduation = models.PositiveSmallIntegerField(
         verbose_name='rok maturity')
@@ -125,28 +92,55 @@ class User(AbstractBaseUser, PermissionsMixin):
         na začiatku s predvoľbou.'
     )
 
-    last_login = models.DateTimeField(
-        verbose_name='posledné prihlásenie',
-        blank=True,
-        null=True
-    )
-
-    is_active = models.BooleanField(
-        default=True,
-        verbose_name='je aktívny'
-    )
-    is_staff = models.BooleanField(
-        default=False,
-        verbose_name='správcovský prístup'
-    )
-
-    USERNAME_FIELD = 'email'
-
-    objects = UserManager()
-
-    class Meta:
-        verbose_name = 'používateľ'
-        verbose_name_plural = 'používatelia'
-
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class County(models.Model):
+    class Meta:
+        verbose_name = 'kraj'
+        verbose_name_plural = 'kraje'
+
+    code = models.AutoField(primary_key=True, verbose_name='kód')
+    name = models.CharField(max_length=30, verbose_name='názov')
+
+    def __str__(self):
+        return self.name
+
+
+class District(models.Model):
+    class Meta:
+        verbose_name = 'okres'
+        verbose_name_plural = 'okresy'
+
+    code = models.AutoField(primary_key=True, verbose_name='kód')
+    name = models.CharField(max_length=30, verbose_name='názov')
+
+    county = models.ForeignKey(County, on_delete=models.CASCADE)
+    abbreviation = models.CharField(max_length=2, verbose_name='skratka')
+
+    def __str__(self):
+        return self.name
+
+
+class School(models.Model):
+    class Meta:
+        verbose_name = 'škola'
+        verbose_name_plural = 'školy'
+
+    code = models.AutoField(primary_key=True, verbose_name='kód')
+    name = models.CharField(max_length=100, verbose_name='názov')
+    abbreviation = models.CharField(max_length=10, verbose_name='skratka')
+
+    street = models.CharField(max_length=100, verbose_name='ulica')
+    city = models.CharField(max_length=100, verbose_name='obec')
+    zip_code = models.CharField(max_length=6, verbose_name='PSČ')
+    email = models.CharField(max_length=50, verbose_name='email', null=True)
+
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{ self.name }, { self.street }, { self.city }'
+
+    def stitok(self):
+        return f'\stitok{{{ self.nazov }}}{{{ self.city }}}{{{ self.zip }}}{{{ self.street }}}'
