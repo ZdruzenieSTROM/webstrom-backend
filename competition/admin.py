341bc8bcdc4e906ec28fc3194718f42898f60d4f
kovacspe <pet.kovacs1@gmail.com>
2020-04-09 17:49:58 +0200

    Registrované modely Competition,Semester,Serie a Problem do Admina

--- competition/admin.py
+++ competition/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from competition.models import Competition, Semester, Serie, Problem
 
 # Register your models here.
+
+admin.site.register(Competition)
+admin.site.register(Semester)
+admin.site.register(Serie)
+admin.site.register(Problem)
