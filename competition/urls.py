48b269d8db2da697890169897880f7262f83764f
kovacspe <pet.kovacs1@gmail.com>
2020-04-12 12:01:47 +0200

    sfunkcnenie prelinkov medyi sériami seminára

--- competition/urls.py
+++ competition/urls.py
@@ -6,9 +6,5 @@ from competition.views import SemesterProblemsView
 app_name = 'competition'
 
 urlpatterns = [
-    #path('register/', register, name='register'),
-    #path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
-
-    #path('district/<int:pk>/', district_by_county, name='filter_district'),
-    path('semester/<int:pk>/', SemesterProblemsView.as_view(), name='semester_problem_views'),
+    path('semester/<int:pk>/', SemesterProblemsView.as_view(), name='semester-problems-detail'),
 ]
