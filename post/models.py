f9df118ceda7940b69b179b5f16e8dd70e996b9e
kovacspe <pet.kovacs1@gmail.com>
2020-04-15 16:19:13 +0200

    Upraveny autor a cas pridania a modifikacie

--- post/models.py
+++ post/models.py
@@ -3,8 +3,6 @@ from django.contrib.sites.models import Site
 
 # Create your models here.
 
-@with_author
-@with_timestamp
 class Post(models.Model):
     class Meta:
         verbose_name = 'príspevok'
@@ -15,27 +13,47 @@ class Post(models.Model):
     title = models.CharField(
         max_length = 64,
         verbose_name = 'nadpis'
-    )
+        )
     text = models.TextField(
         verbose_name='text príspevku'
-    )
+        )
     img_url = models.URLField(
         verbose_name='adresa obrázku'
-    )
+        )
     post_template = models.FileField(
         verbose_name='šablóna príspevku'
-    )
+        )
 
     published = models.BooleanField(
         default = False,
         verbose_name = 'viditeľný'
-    )
+        )
     # Tu by som nejak možno zapracoval aby ich nebolo pinnutých veľa, prípadne môžeme to aj úplne vypustiť aale príde mi to useful
     pinned = models.BooleanField(
         default = False,
         verbose_name = 'pripnúť príspevok',
         help_text = 'Príspevok bude zobrazený vždy na vrchu feedu'
-    )
+        )
+
+    created_at = models.DateTimeField(
+        verbose_name='dátum a čas vytvorenia',
+        auto_now_add=True,
+        editable=False
+        )
+    modified_at = models.DateTimeField(
+        verbose_name='dátum a čas poslednej úpravy',
+        auto_now=True,
+        editable=False
+        )
+
+    created_by = models.ForeignKey(
+        'user.User',
+        verbose_name='autor',
+        null=True,
+        blank=True,
+        editable=False,
+        on_delete=models.SET_NULL
+        )
 
     def __str__(self):
         return self.title
