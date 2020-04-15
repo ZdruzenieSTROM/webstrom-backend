0075f0534bc72d8453f6b9d88e02220476b6a311
samoKrajci <samo.krajci@gmail.com>
2020-04-15 22:37:21 +0200

    menu update

--- post/urls.py
+++ post/urls.py
@@ -5,6 +5,6 @@ from post.views import PostDetailView, PostListView
 app_name = 'post'
 
 urlpatterns = [
-    path('post/<int:pk>/', PostDetailView.as_view(), name=''),
-    path('post-feed/', PostListView.as_view(), name='')
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail-view'),
+    path('post-feed/', PostListView.as_view(), name='post-list-view')
 ]
