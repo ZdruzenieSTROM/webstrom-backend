f7220fd4aa9c12d7903c68f630174913dbcec9ca
samoKrajci <samo.krajci@gmail.com>
2020-04-19 15:51:56 +0200

    vytvoreny archiv a detail-view semestra; mensi update menu

--- competition/urls.py
+++ competition/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
 
-from competition.views import LatestSemesterView, SemesterProblemsView
+from competition.views import (
+    LatestSemesterView,
+    SemesterProblemsView,
+    SeriesProblemsView,
+    ArchiveView,
+    SemesterDetailView
+)
 
 app_name = 'competition'
 
 urlpatterns = [
-    path('semester/<int:pk>/', SemesterProblemsView.as_view(),
+    path('semester/<int:pk>/problems', SemesterProblemsView.as_view(),
          name='semester-problems-detail'),
     path('semester-last', LatestSemesterView.as_view(),
-         name='latest-semester-view')
+         name='latest-semester-view'),
+    path('series/<int:pk>/', SeriesProblemsView.as_view(),
+         name='series-problems-detail'),
+    path('archive', ArchiveView.as_view(),
+         name='archive-view'),
+    path('semester/<int:pk>/', SemesterDetailView.as_view(),
+         name='semester-detail'),
 ]
--- competition/views.py
+++ competition/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView, ListView
 
 from competition.models import Competition, Semester, Series
 
@@ -26,3 +26,48 @@ class SemesterProblemsView(DetailView):
 class LatestSemesterView(SemesterProblemsView):
     def get_object(self):
         return Competition.get_by_current_site().semester_set.order_by('-end').first()
+
+
+class SeriesProblemsView(DetailView):
+    template_name = 'competition/series_problems.html'
+    model = Series
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['series'] = self.object
+        context['problems'] = {}
+        context['problems'] = self.object.problem_set.order_by('order')
+
+        return context
+
+
+class ArchiveView(ListView):
+    template_name = 'competition/archive.html'
+    model = Semester
+    context_object_name = 'context'
+
+    def get_queryset(self):
+        site_competition = Competition.get_by_current_site()
+        years = {}
+        for sem in Semester.objects.filter(competition=site_competition).order_by('-year'):
+            try:
+                years[sem.year]
+            except KeyError:
+                years[sem.year] = []
+            years[sem.year].append(sem)
+            pass
+        return years
+
+
+class SemesterDetailView(DetailView):
+    template_name = 'competition/semester_detail.html'
+    model = Semester
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['semester'] = self.object
+        context['series'] = Series.objects.filter(
+            semester=self.object)
+        context['competition_semesters'] = self.object.competition.semester_set.all()
+
+        return context
