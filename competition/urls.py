e43dfcb238c43ee8c3870cb199512d3d6cf47f44
Martin Mihálik <34107535+mmihalik@users.noreply.github.com>
2020-04-20 14:55:12 +0200

    Nejaký refactor a základné opravy

--- competition/urls.py
+++ competition/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 
-from competition.views import (
-    LatestSemesterView,
-    SemesterProblemsView,
-    SeriesProblemsView,
-    ArchiveView,
-    SemesterDetailView
-)
+from competition.views import (ArchiveView, LatestSemesterView,
+                               SemesterDetailView, SemesterProblemsView,
+                               SeriesProblemsView)
 
 app_name = 'competition'
 
--- competition/views.py
+++ competition/views.py
@@ -6,12 +6,11 @@ from competition.models import Competition, Semester, Series
 class SemesterProblemsView(DetailView):
     template_name = 'competition/semester_problems.html'
     model = Semester
+    context_object_name = 'semester'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['semester'] = self.object
-        context['series'] = Series.objects.filter(
-            semester=self.object)
+        context['series'] = Series.objects.filter(semester=self.object)
         context['problems'] = {}
 
         for series in context['series']:
@@ -24,24 +23,24 @@ class SemesterProblemsView(DetailView):
 
 
 class LatestSemesterView(SemesterProblemsView):
-    def get_object(self):
+    def get_object(self, queryset=None):
         return Competition.get_by_current_site().semester_set.order_by('-end').first()
 
 
 class SeriesProblemsView(DetailView):
     template_name = 'competition/series_problems.html'
     model = Series
+    context_object_name = 'series'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['series'] = self.object
-        context['problems'] = {}
         context['problems'] = self.object.problem_set.order_by('order')
 
         return context
 
 
 class ArchiveView(ListView):
+    # toto sa mi vôbec nepozdáva
     template_name = 'competition/archive.html'
     model = Semester
     context_object_name = 'context'
@@ -49,25 +48,18 @@ class ArchiveView(ListView):
     def get_queryset(self):
         site_competition = Competition.get_by_current_site()
         years = {}
+
         for sem in Semester.objects.filter(competition=site_competition).order_by('-year'):
             try:
                 years[sem.year]
             except KeyError:
                 years[sem.year] = []
             years[sem.year].append(sem)
-            pass
+
         return years
 
 
 class SemesterDetailView(DetailView):
     template_name = 'competition/semester_detail.html'
     model = Semester
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['semester'] = self.object
-        context['series'] = Series.objects.filter(
-            semester=self.object)
-        context['competition_semesters'] = self.object.competition.semester_set.all()
-
-        return context
+    context_object_name = 'semester'
