bd9acf746fe7bbc7e105f3d974c10457396d8f7f
kovacspe <pet.kovacs1@gmail.com>
2020-04-27 01:39:18 +0200

    Registrácia modelov do admina

--- competition/admin.py
+++ competition/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from competition.models import Competition, Problem, Semester, Series
+from competition.models import Competition, Event, Grade, Problem, Semester, Series, UserEventRegistration, Publication, Solution, LateTag
 
 admin.site.register(Competition)
+admin.site.register(Event)
+admin.site.register(Grade)
 admin.site.register(Semester)
 admin.site.register(Series)
 admin.site.register(Problem)
+admin.site.register(Publication)
+admin.site.register(UserEventRegistration)
+admin.site.register(Solution)
+admin.site.register(LateTag)
