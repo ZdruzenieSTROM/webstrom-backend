d0521af6e8ca951d099bf344f81fd03f6a83dce6
Matúš Hlaváčik <hlavacik.matus@gmail.com>
2020-05-03 22:00:18 +0200

    uprava na zaklade zmeny ID rocnika pre nestudenta

--- user/static/user/register.js
+++ user/static/user/register.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     if ($('#id_school_not').is(":checked")) {
         $("#id_school_not").change();
     } else {
-        $('#id_grade option[value="-1"]').attr('disabled', true);
+        $('#id_grade option[value="13"]').attr('disabled', true);
     }
 });
 
@@ -29,8 +29,8 @@ $("#id_school_not").change(function () {
     if ($(this).is(":checked")) {
         $('#div_id_school_info').hide();
         $("#id_school").val(1);
-        $('#id_grade option[value="-1"]').attr('disabled', false);
-        $("#id_grade").val(-1);
+        $('#id_grade option[value="13"]').attr('disabled', false);
+        $("#id_grade").val(13);
         $("#id_school_name").val('Bez školy');
         $("#id_county").prop('disabled', true);
         $("#id_district").prop('disabled', true);
@@ -42,8 +42,8 @@ $("#id_school_not").change(function () {
         $("#id_school_name").val(null);
         $("#id_school_name").prop('disabled', false);
         $('#id_grade option:not(:selected)').attr('disabled', false);
-        $('#id_grade option[value="-1"]').attr('disabled', true);
-        $("#id_grade").val(0);
+        $('#id_grade option[value="13"]').attr('disabled', true);
+        $("#id_grade").val(null);
     }
 })
 
