f15cea09b3a2921d3ac31f22cd101f4b784c9b85
Martin Mihálik <34107535+mmihalik@users.noreply.github.com>
2020-05-11 16:03:30 +0200

    Ručné generovanie náhľadu

--- base/utils.py
+++ base/utils.py
@@ -5,4 +5,7 @@ from django.core.files import File
 def mime_type(file: File) -> str:
     """Zistí mime type zadaného súboru"""
 
+    # Podľa dokumentácie python-magic by mali prvé dva kB
+    # spoľahlivo stačiť na určenie typu
+    file.open(mode='rb')
     return magic.from_buffer(file.read(2048), mime=True)
--- competition/admin.py
+++ competition/admin.py
@@ -1,8 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
 
 from competition.models import (Competition, Event, Grade, LateTag, Problem,
                                 Publication, Semester, Series, Solution,
                                 UserEventRegistration)
+
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    change_form_template = 'competition/admin/publication_change.html'
+
+    def response_change(self, request, obj):
+        if 'generate-thumbnail' in request.POST:
+            obj.generate_thumbnail(forced=True)
+
+            if obj.thumbnail:
+                self.message_user(request, 'Náhľad bol vygenerovaný')
+            else:
+                self.message_user(
+                    request, 'Náhľad sa nepodarilo vygenerovať', level=messages.ERROR)
+
+            return HttpResponseRedirect('.')
+
+        return super(PublicationAdmin, self).response_change(request, obj)
+
 
 admin.site.register(Competition)
 admin.site.register(Event)
@@ -10,7 +31,6 @@ admin.site.register(Grade)
 admin.site.register(Semester)
 admin.site.register(Series)
 admin.site.register(Problem)
-admin.site.register(Publication)
 admin.site.register(UserEventRegistration)
 admin.site.register(Solution)
 admin.site.register(LateTag)
