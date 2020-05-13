a44cdf450ba6c59a583aa7b1fc3a70d639de683a
Martin Mihálik <34107535+mmihalik@users.noreply.github.com>
2020-05-13 16:54:08 +0200

    Reenablnute profiles a users a doopravene profiles

--- base/management/commands/restoredb.py
+++ base/management/commands/restoredb.py
@@ -53,7 +53,7 @@ class Command(BaseCommand):
         load_fixture('sites', 'flatpages')
 
         load_fixture('counties', 'districts', 'schools', 'schools_custom')
-        load_fixture('superuser')  # , 'users', 'profiles')
+        load_fixture('superuser', 'users', 'profiles')
         # load_fixture('users_random', 'profiles_random')
 
         # load_fixture('competitions', 'grades', 'late_tags')
