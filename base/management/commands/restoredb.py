b1dd67694375b1c2c86b07a20ec801ff33f38440
Martin Mihálik <34107535+mmihalik@users.noreply.github.com>
2020-05-13 17:03:41 +0200

    Povolené zvyšné fixtures v restoredb

--- base/management/commands/restoredb.py
+++ base/management/commands/restoredb.py
@@ -56,6 +56,6 @@ class Command(BaseCommand):
         load_fixture('superuser', 'users', 'profiles')
         load_fixture('users_random', 'profiles_random')
 
-        # load_fixture('competitions', 'grades', 'late_tags')
-        # load_fixture('semesters', 'event_registrations')
-        # load_fixture('event_registrations_random', 'solutions_random')
+        load_fixture('competitions', 'grades', 'late_tags')
+        load_fixture('semesters', 'event_registrations')
+        load_fixture('event_registrations_random', 'solutions_random')
