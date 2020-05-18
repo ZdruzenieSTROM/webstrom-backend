7417891b27d17fb658d7c3b8da4349e926b7f163
samoKrajci <samo.krajci@gmail.com>
2020-05-18 19:09:46 +0200

    score_pretty filter fix - zobrazuje nulu

--- competition/templatetags/competition_extras.py
+++ competition/templatetags/competition_extras.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter(name='score_pretty')
 def score_pretty(solution):
-    return '?' if not solution.score else solution.score
+    return '?' if solution.score is None else solution.score
 
 @register.filter
 def batch(iterable, n=1):
