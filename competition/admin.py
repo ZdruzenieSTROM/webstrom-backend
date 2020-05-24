30eb36f7666e18b17d24687671f6249c2c834694
samoKrajci <samo.krajci@gmail.com>
2020-05-24 21:53:52 +0200

    pridane stlpce a filtre pre vsetko z competition modelu

--- competition/admin.py
+++ competition/admin.py
@@ -6,22 +6,108 @@ from django.db.models import Value, CharField, F
 from competition.models import (Competition, Event, EventRegistration, Grade,
                                 LateTag, Problem, Publication, School,
                                 Semester, Series, Solution)
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'competition',
+        'semester',
+        'order',
+        'deadline',
+        'active',
+        'complete',
+    )
+
+    list_filter = (
+        'semester__competition',
+        'complete',
+    )
+
+    def active(self, obj):
+        return not obj.is_past_deadline
+    active.boolean = True
+
+    def competition(self, obj):
+        return obj.semester.competition
+    active.admin_order_field = 'semester__competition'
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'competition',
+        'year',
+        'school_year',
+        'start',
+        'end',
+    )
+
+    list_filter = (
+        'competition',
+        'school_year'
+    )
+
+
+@admin.register(Semester)
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'competition',
+        'year',
+        'school_year',
+        'season_code',
+        'start',
+        'end',
+    )
+
+    list_filter = (
+        'competition',
+        'school_year',
+        'season_code',
+    )
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
-    list_display = ['name', 'street', 'city', 'abbreviation']
+    list_display = (
+        'name', 
+        'street', 
+        'city', 
+        'abbreviation',
+    )
 
 
 @admin.register(Problem)
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'order', 'series', 'get_text']
+    list_display = (
+        '__str__', 
+        'order', 
+        'series', 
+        'get_text',
+    )
     def get_text(self, obj):
         return obj.text[:70]+'...'
 
 @admin.register(Solution)
 class SolutionAdmin(admin.ModelAdmin):
-    list_display = ['solutionName', 'problem', 'semester_registration', 'uploaded_at', 'late_tag', 'is_online', 'score']
-    list_editable = ['score']
+    
+    list_display = (
+        'solutionName', 
+        'problem', 
+        'semester_registration', 
+        'uploaded_at', 
+        'late_tag', 
+        'is_online', 
+        'score'
+    )
+    list_editable = ('score',)
+
+    list_filter = (
+        'semester_registration__event__competition', 
+        'late_tag', 
+        'is_online', 
+        'score',
+    )
 
     date_hierarchy = 'uploaded_at'
 
@@ -32,14 +118,33 @@ class SolutionAdmin(admin.ModelAdmin):
 class PublicationAdmin(admin.ModelAdmin):
     change_form_template = 'competition/admin/publication_change.html'
 
-    list_display = ['thumb', 'name', 'comp', 'event', 'order']
-    ordering = ['event', '-order']
+    list_display = (
+        'name', 
+        'thumb', 
+        'competition', 
+        'event', 
+        'order', 
+        'school_year',
+    )
+    ordering = (
+        'event', 
+        '-order',
+    )
 
-    list_filter = ['event']
+    list_filter = (
+        'event__school_year', 
+        'event__competition',
+    )
 
-    def comp(self, obj):
+    def school_year(self, obj):
+        return obj.event.school_year
+    school_year.admin_order_field = 'event__school_year'
+
+    def competition(self, obj):
         return obj.event.competition
-    comp.__name__ = 'competition'
+    competition.__name__ = 'competition'
+    competition.admin_order_field = 'event__competition'
+    #comp.admin_filter_field = 'event__competition'
 
 
     def thumb(self, obj):
@@ -71,10 +176,7 @@ class PublicationAdmin(admin.ModelAdmin):
         return super(PublicationAdmin, self).response_change(request, obj)
 
 
-admin.site.register(Event)
-admin.site.register(Semester)
-admin.site.register(Series)
-admin.site.register(EventRegistration)
+#admin.site.register(EventRegistration)
 #admin.site.register(Competition)
 #admin.site.register(Grade)
 #admin.site.register(LateTag)
