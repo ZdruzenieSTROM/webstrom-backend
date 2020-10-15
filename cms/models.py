b55f912185c9f54ba7f670755d8eb024e4a5e0db
kovacspe <pet.kovacs1@gmail.com>
2020-10-15 22:49:50 +0200

    Pridaný model MenuItem

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,4 @@
+from django.contrib.sites.models import Site
 from django.db import models
 from django.utils.timezone import now
 
@@ -51,3 +52,20 @@ class PostLink(models.Model):
 
     def __str__(self):
         return f'{self.post}-{self.caption}'
+
+
+class MenuItem(models.Model):
+    class Meta:
+        verbose_name = 'položka v menu'
+        verbose_name_plural = 'položky v menu'
+
+    caption = models.CharField(
+        verbose_name='názov',
+        help_text='Nápis, ktorý sa zobrazí v menu. Maximálne 2 slová.',
+        max_length=25)
+    url = models.URLField(verbose_name='URL',
+                          help_text='URL stránky kam má preklik viesť')
+    priority = models.SmallIntegerField(
+        verbose_name='priorita',
+        help_text='Priorita, čím väčšie, tým vyššie v menu.')
+    sites = models.ManyToManyField(Site)
