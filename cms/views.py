a7fc08714f78bc21e2bd2262eb4b4daddf6ef4be
kovacspe <pet.kovacs1@gmail.com>
2020-10-16 00:22:46 +0200

    Pridané url, view a serializers pre model MenuItem

--- cms/views.py
+++ cms/views.py
@@ -1,9 +1,31 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from cms.models import Post
-from cms.serializers import PostSerializer
-from cms.permissions import UserPermission
+from cms.models import MenuItem, Post
+from cms.serializers import MenuItemShortSerializer, PostSerializer
+from cms.permissions import PostPermission, ReadOnly
+
+
+class MenuItemViewSet(viewsets.ModelViewSet):
+    """
+    Obsluhuje API endpoint pre MenuItems
+    """
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemShortSerializer
+    permission_classes = [ReadOnly]
+
+    @action(methods=['get'], detail=False, url_path=r'on_site/(?P<site_id>\d+)')
+    def on_site(self, request, site_id):
+        items = MenuItem.objects.filter(sites=site_id).all()
+        serializer = MenuItemShortSerializer(items, many=True)
+        return Response(serializer.data)
+
+    @action(methods=['get'], detail=False)
+    def on_current_site(self, request):
+        items = MenuItem.objects.filter(
+            sites=request.site).order_by('-priority').all()
+        serializer = MenuItemShortSerializer(items, many=True)
+        return Response(serializer.data)
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -12,7 +34,7 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (UserPermission,)
+    permission_classes = (PostPermission,)
 
     @action(detail=False)
     def visible(self, request):
