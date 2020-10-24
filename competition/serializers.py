c1027e85bdf6ccfa588697b8302c55e405147d9c
kovacspe <pet.kovacs1@gmail.com>
2020-10-24 21:35:01 +0200

    Pridané serializeri pre Problem a Series

---
+++ competition/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from competition import models
+
+
+class ProblemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Problem
+        fields = '__all__'
+
+
+class SeriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Series
+        exclude = ['sum_method']
+
+
+class SeriesWithProblemsSerializer(serializers.ModelSerializer):
+    problems = ProblemSerializer(many=True)
+
+    class Meta:
+        model = models.Series
+        exclude = ['sum_method']
+
+    def create(self, validated_data):
+        problem_data = validated_data.pop('problems')
+        series = models.Series.objects.create(**validated_data)
+        for data in problem_data:
+            models.Problem.objects.create(series=series, **data)
+        return series
