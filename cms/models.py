847b0375cb8d5b1a413175a1fd2e7df66dc3a69e
kovacspe <pet.kovacs1@gmail.com>
2020-10-25 18:00:11 +0100

    Nahradený URL field za CharField, pre podporu relativnych linkov

--- cms/models.py
+++ cms/models.py
@@ -47,8 +47,8 @@ class PostLink(models.Model):
         verbose_name='názov',
         help_text='Nápis, ktorý po kliknutí presmeruje na link. Maximálne 2 slová.',
         max_length=25)
-    url = models.URLField(verbose_name='URL',
-                          help_text='URL stránky kam má preklik viesť')
+    url = models.CharField(verbose_name='URL', max_length=100,
+                           help_text='URL stránky kam má preklik viesť')
 
     def __str__(self):
         return f'{self.post}-{self.caption}'
@@ -64,8 +64,9 @@ class MenuItem(models.Model):
         verbose_name='názov',
         help_text='Nápis, ktorý sa zobrazí v menu. Maximálne 2 slová.',
         max_length=25)
-    url = models.URLField(verbose_name='URL',
-                          help_text='URL stránky kam má preklik viesť')
+    url = models.CharField(verbose_name='URL',
+                           max_length='100',
+                           help_text='URL stránky kam má preklik viesť')
     priority = models.SmallIntegerField(
         verbose_name='priorita',
         help_text='Priorita, čím väčšie, tým vyššie v menu.')
