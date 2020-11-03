6789bb2ee846d97d39e08f5baa5d1e0eee494d33
kovacspe <pet.kovacs1@gmail.com>
2020-11-04 00:20:17 +0100

    Zakomentované kryjúce sa urls

--- competition/urls.py
+++ competition/urls.py
@@ -26,14 +26,16 @@ urlpatterns = [
          name='latest-series-problems'),
 
     # Výsledky
-    path('series/<int:pk>/results', SeriesResultsView.as_view(),
-         name='series-results'),
-    path('series/<int:pk>/results/latex', SeriesResultsLatexView.as_view(),
-         name='series-results-latex'),
+
+    #    path('series/<int:pk>/results', SeriesResultsView.as_view(),
+    #         name='series-results'),
+    #    path('series/<int:pk>/results/latex', SeriesResultsLatexView.as_view(),
+    #         name='series-results-latex'),
     path('semester/<int:pk>/results', SemesterResultsView.as_view(),
          name='semester-results'),
     path('semester/<int:pk>/results/latex', SemesterResultsLatexView.as_view(),
          name='semester-results-latex'),
+
 
     # Registrácia do semestra
     path('semester/<int:pk>/register/<path:cont>', SemesterRegistrationView.as_view(),
