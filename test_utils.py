21a6c80ca31f8f5df8da3468c708d0c81a0db5a2
samoKrajci <samo.krajci@gmail.com>
2020-11-13 13:45:57 +0100

    add test_utils

---
+++ test_utils.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+from webstrom.settings import BASE_DIR
+
+
+def get_app_fixtures(app_list=[]):
+    '''returns all .json fixtures from given list of apps'''
+    fixtures = []
+
+    for app in app_list:
+        fixtures_dir = os.path.join(BASE_DIR, app, 'fixtures')
+        fixtures_files = map(str, Path(fixtures_dir).rglob('*.json'))
+        for file in fixtures_files:
+            print(file)
+
+            fixtures.append(file)
+
+    return fixtures
