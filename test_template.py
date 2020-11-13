e641dccc39529fb82ff1c4d0ce19ee7f5eb8b6a6
samoKrajci <samo.krajci@gmail.com>
2020-11-13 13:47:23 +0100

    add fixtures to test_template

--- test_template.py
+++ test_template.py
@@ -4,11 +4,14 @@ more info  here https://docs.djangoproject.com/en/3.1/topics/testing/overview/
 you run them by running "./manage.py test"
 '''
 from django.test import TestCase
+from rest_framework.test import APIClient
+from test_utils import get_app_fixtures
 
 
 class TestTest(TestCase):
     '''
     test class must be a subclass of django.test.TestCase
+        or another suitable class; visit docs for more
     add fixtures to fill the db
     '''
     fixtures = get_app_fixtures([
@@ -17,10 +20,14 @@ class TestTest(TestCase):
     ])
 
     def setUp(self):
-        '''setUp method is called once before other tests'''
+        '''
+        setUp method is called once before other tests
+        most of the time we want to use APIClient
+        '''
+        self.client = APIClient()
 
     def test_name_must_start_with_test(self):
-        '''name of test method must start with "test_"'''
+        '''name of test method must start with "test"'''
         self.assertFalse(1 + 2 == 300)
 
     def test_another_one(self):
