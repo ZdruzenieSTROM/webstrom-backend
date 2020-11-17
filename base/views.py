25500fbfa74828da46ea376b777b04f3f9648c04
kovacspe <pet.kovacs1@gmail.com>
2020-11-17 12:17:25 +0100

    Hľadanie stránky podľa url

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.contrib.flatpages.models import FlatPage
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from base.serializers import FlatPageSerializer
 # Create your views here.
 
@@ -10,3 +12,9 @@ class FlatPageViewSet(viewsets.ModelViewSet):
     """
     queryset = FlatPage.objects.all()
     serializer_class = FlatPageSerializer
+
+    @action(methods=['get'], detail=False, url_path=r'by-url(?P<page_url>.+)')
+    def by_url(self, request, page_url):
+        page = FlatPage.objects.filter(url=page_url).first()
+        serializer = FlatPageSerializer(page)
+        return Response(serializer.data, status=status.HTTP_200_OK)
