5ca6519807bc200815d1b0aca2da0bf143ae4fc9
Tomas Kocak <tomas.kocak@gmail.com>
2020-12-06 22:08:27 +0100

    Pridaná aj session authentikacia

--- webstrom/settings.py
+++ webstrom/settings.py
@@ -98,6 +98,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
