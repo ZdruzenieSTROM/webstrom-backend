a5f2a0026f3d0ea3ff7ad3b5bd86a91ed579b73c
Tomas Kocak <tomas.kocak@gmail.com>
2020-12-19 12:26:51 +0100

    add user/password/change/ api endpoint

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.forms import SetPasswordForm
 
 from rest_framework import serializers, exceptions
 
@@ -163,6 +164,51 @@ class RegisterSerializer(serializers.Serializer):
 class VerifyEmailSerializer(serializers.Serializer):
     #pylint: disable=w0223
     key = serializers.CharField()
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    # pylint: disable=w0223
+    # pylint: disable=w0221
+    # pylint: disable=w0201
+    old_password = serializers.CharField(max_length=128)
+    new_password1 = serializers.CharField(max_length=128)
+    new_password2 = serializers.CharField(max_length=128)
+
+    set_password_form_class = SetPasswordForm
+
+    def __init__(self, *args, **kwargs):
+        self.old_password_field_enabled = True
+        super().__init__(*args, **kwargs)
+
+        if not self.old_password_field_enabled:
+            self.fields.pop('old_password')
+
+        self.request = self.context.get('request')
+        self.user = getattr(self.request, 'user', None)
+
+    def validate_old_password(self, value):
+        invalid_password_conditions = (
+            self.old_password_field_enabled,
+            self.user,
+            not self.user.check_password(value)
+        )
+
+        if all(invalid_password_conditions):
+            err_msg = "Staré heslo bolo zadané nesprávne."
+            raise serializers.ValidationError(err_msg)
+        return value
+
+    def validate(self, attrs):
+        self.set_password_form = self.set_password_form_class(
+            user=self.user, data=attrs
+        )
+
+        if not self.set_password_form.is_valid():
+            raise serializers.ValidationError(self.set_password_form.errors)
+        return attrs
+
+    def save(self):
+        self.set_password_form.save()
 
 
 class UserShortSerializer(serializers.ModelSerializer):
