bed0a56b4d61517a293e6bba62e39b1f9224662d
Tomas Kocak <tomas.kocak@gmail.com>
2020-12-19 15:41:06 +0100

    change user/logout from get to post

--- user/views.py
+++ user/views.py
@@ -94,8 +94,10 @@ class LogoutView(APIView):
     permission_classes = (AllowAny,)
 
     def get(self, request, *args, **kwargs):
-        response = self.logout(request)
-        return self.finalize_response(request, response, *args, **kwargs)
+        return self.http_method_not_allowed(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.logout(request)
 
     def logout(self, request):
         print(request.user)
