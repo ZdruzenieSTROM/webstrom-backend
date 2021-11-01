ef3973e41abae106b7f863733916719f7898e057
kovacspe <pet.kovacs1@gmail.com>
2021-11-01 22:39:56 +0100

    Add user loading

--- base/management/commands/load_db.py
+++ base/management/commands/load_db.py
@@ -1,8 +1,18 @@
 import datetime
 import sqlite3
 from django.core.management import BaseCommand
-from competition.models import Semester,Series,Problem,Competition
+from django.utils import timezone
+from django.utils.dateparse import parse_datetime
+import pytz
+from allauth.account.utils import user_email, user_username, setup_user_email
+from allauth.account.models import EmailAddress
+
+from competition.models import Semester, Series, Problem, Competition
+from user.models import User
+from personal.models import Profile, School
 from competition.utils import get_school_year_by_date
+
+
 SERIES_QUERY = '''SELECT id,number,submission_deadline, sum_method,season_id
 FROM competitions_series'''
 
@@ -16,26 +26,50 @@ INNER JOIN problems_problemset AS problemset ON series.problemset_id=problemset.
 INNER JOIN problems_probleminset AS inset ON inset.problemset_id=problemset.id
 INNER JOIN problems_problem AS problem ON problem.id=inset.problem_id'''
 
-COMPETITION_ID_MAPPING ={
-    1:Competition.objects.get(pk=1),
-    2:Competition.objects.get(pk=3),
-    3:Competition.objects.get(pk=2)
+COMPETITION_ID_MAPPING = {
+    1: Competition.objects.get(pk=1),
+    2: Competition.objects.get(pk=3),
+    3: Competition.objects.get(pk=2)
 }
 
-SUM_METHOD_DICT={
-    'SUCET_SERIE_35':'',
-    'SUCET_SERIE_32':'',
-    'SUCET_SERIE_MATIK':'',
-    'SUCET_SERIE_MALYNAR':'',
+USERS_QUERY = '''
+SELECT user.email,user.is_staff,user.is_active,user.first_name,user.last_name, user.date_joined,
+        user.username,user.is_superuser,user.password,phone_number,parent_phone_number,
+        classlevel,school.name AS school_name,addr.street AS school_street,addr.city AS school_city
+FROM auth_user AS user
+INNER JOIN profiles_userprofile AS prof ON prof.user_id=user.id
+INNER JOIN schools_school AS school ON prof.school_id=school.id
+INNER JOIN schools_address AS addr ON school.address_id=addr.id
+WHERE email<>''
+'''
+
+SUM_METHOD_DICT = {
+    'SUCET_SERIE_35': '',
+    'SUCET_SERIE_32': '',
+    'SUCET_SERIE_MATIK': '',
+    'SUCET_SERIE_MALYNAR': '',
 }
 
-def to_school_year(year,competition):
-    return get_school_year_by_date(datetime.date(day=1,month=10,year=competition.start_year + year))
+
+def localize(date):
+    return pytz.timezone("Europe/Helsinki").localize(
+        parse_datetime(date),
+        is_dst=None
+    )
+
+
+def to_school_year(year, competition):
+    return get_school_year_by_date(datetime.date(day=1, month=10, year=competition.start_year + year))
+
+
+def estimate_school(user):
+    return School.objects.first()
+
 
 class Command(BaseCommand):
 
-    def _load_semester(self,conn,competition_id_mapping):
-        semester_id_mapping={}
+    def _load_semester(self, conn, competition_id_mapping):
+        semester_id_mapping = {}
         cursor = conn.cursor()
         cursor.execute(SEMESTER_QUERY)
         semesters = cursor.fetchall()
@@ -47,14 +81,14 @@ class Command(BaseCommand):
                 school_year=to_school_year(
                     semester['year'],
                     competition_id_mapping[semester['competition_id']]),
-                start=semester['start'],
-                end=semester['end']
+                start=localize(semester['start']),
+                end=localize(semester['end'])
             )
             new_semester.save()
             semester_id_mapping[semester['id']] = new_semester
         return semester_id_mapping
 
-    def _load_series(self,conn,semester_id_mapping):
+    def _load_series(self, conn, semester_id_mapping):
         series_id_mapping = {}
         cursor = conn.cursor()
         cursor.execute(SERIES_QUERY)
@@ -63,7 +97,7 @@ class Command(BaseCommand):
             new_series = Series(
                 semester=semester_id_mapping[series['season_id']],
                 order=series['number'],
-                deadline=series['submission_deadline'],
+                deadline=localize(series['submission_deadline']),
                 complete=False,
                 sum_method=SUM_METHOD_DICT[series['sum_method']]
 
@@ -72,7 +106,7 @@ class Command(BaseCommand):
             series_id_mapping[series['id']] = new_series
         return series_id_mapping
 
-    def _load_problems(self,conn,series_id_mapping):
+    def _load_problems(self, conn, series_id_mapping):
         problem_id_mapping = {}
         cursor = conn.cursor()
         cursor.execute(PROBLEM_QUERY)
@@ -87,26 +121,61 @@ class Command(BaseCommand):
             problem_id_mapping[problem['id']] = new_problem
         return problem_id_mapping
 
-    def _load_competitions(self,conn):
-        semester_id_mapping = self._load_semester(conn,COMPETITION_ID_MAPPING)
-        series_id_mapping = self._load_series(conn,semester_id_mapping)
-        problem_id_mapping = self._load_problems(conn,series_id_mapping)
+    def _load_competitions(self, conn):
+        semester_id_mapping = self._load_semester(conn, COMPETITION_ID_MAPPING)
+        series_id_mapping = self._load_series(conn, semester_id_mapping)
+        problem_id_mapping = self._load_problems(conn, series_id_mapping)
+        return semester_id_mapping, series_id_mapping, problem_id_mapping
 
-    def _load_users(self,conn):
+    def _load_users(self, conn):
+        cursor = conn.cursor()
+        cursor.execute(USERS_QUERY)
+        users = cursor.fetchall()
+        for user in users:
+            new_user = User.objects.create_user(
+                email=user['email'],
+                verified_email=True,
+                is_staff=user['is_staff'],
+                is_active=user['is_active'],
+                date_joined=localize(user['date_joined']),
+
+            )
+            email_address = EmailAddress(
+                user=new_user,
+                email=user['email'],
+                verified=True,
+                primary=True
+            )
+            email_address.save()
+            new_user.password = user['password']
+            # new_user.set_password(user['password'])
+            new_user.save()
+            school = estimate_school(user)
+            profile = Profile(
+                first_name=user['first_name'],
+                last_name=user['last_name'],
+                user=new_user,
+                nickname=user['username'],
+                school=school,
+                year_of_graduation=2020,
+                phone=user['phone_number'],
+                parent_phone=user['parent_phone_number'],
+                gdpr=True
+            )
+            profile.save()
+
+    def _load_results(self, conn):
         pass
-
-    def _load_results(self,conn):
-        pass
-
 
     def add_arguments(self, parser):
         # Positional arguments
         parser.add_argument('db', type=str)
 
     def handle(self, *args, **options):
-        conn=None
+        conn = None
         try:
             conn = sqlite3.connect(options['db'])
+
             def dict_factory(cursor, row):
                 row_dict = {}
                 for idx, col in enumerate(cursor.description):
@@ -116,8 +185,6 @@ class Command(BaseCommand):
             self._load_competitions(conn)
             self._load_users(conn)
             self._load_results(conn)
-        except:
-            raise
         finally:
             if conn:
                 conn.close()
