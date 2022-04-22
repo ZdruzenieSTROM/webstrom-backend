a0bb3790b30dfb824428d3018d57b165a0f742b8
kovacspe <pet.kovacs1@gmail.com>
2022-04-23 00:31:53 +0200

    lint

--- competition/serializers.py
+++ competition/serializers.py
@@ -116,8 +116,7 @@ class CommentSerializer(serializers.ModelSerializer):
     edit_allowed = serializers.SerializerMethodField('can_edit')
 
     def get_posted_by_name(self, obj):
-        if hasattr(obj.user, 'profile'):
-            return self.user.get_full_name()
+        return obj.user.get_full_name()
 
     def can_edit(self, obj):
         if 'request' in self.context:
