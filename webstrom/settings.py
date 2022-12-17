1fb788f335600ac78bc2498d6a3fc79e1d67e613
Martin Mihálik <mihalik.martin@outlook.sk>
2022-12-17 17:19:01 +0100

    Rethink dependencies

--- webstrom/settings.py
+++ webstrom/settings.py
@@ -101,6 +101,8 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
