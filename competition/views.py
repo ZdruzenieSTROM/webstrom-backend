bcef79063643a1bfe0217190ac8ce8d7d1bfed27
Viktória Brezinová <vikibrezinova@gmail.com>
2023-05-27 13:41:47 +0200

    add publication type and order to uploading publications

--- competition/views.py
+++ competition/views.py
@@ -15,8 +15,9 @@ from rest_framework.response import Response
 from base.utils import mime_type
 from competition import utils
 from competition.models import (Comment, Competition, Event, EventRegistration,
-                                Grade, LateTag, Problem, Publication, Semester,
-                                Series, Solution, Vote)
+                                Grade, LateTag, Problem, Publication,
+                                PublicationType, Semester, Series, Solution,
+                                Vote)
 from competition.permissions import (CommentPermission,
                                      CompetitionRestrictedPermission,
                                      ProblemPermission)
@@ -722,7 +723,7 @@ class EventRegistrationViewSet(viewsets.ModelViewSet):
 
 
 class PublicationViewSet(viewsets.ModelViewSet):
-    """Publikácie(výsledky, brožúrky ... nie časopis)"""
+    """Publikácie(výsledky, brožúrky, časopisy, ...)"""
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
     permission_classes = (CompetitionRestrictedPermission,)
@@ -758,10 +759,15 @@ class PublicationViewSet(viewsets.ModelViewSet):
         if mime_type(file) not in ['application/pdf', 'application/zip']:
             raise exceptions.ParseError(detail='Nesprávny formát')
 
-        event = Event.objects.filter(pk=request.data['event']).first()
+        event = Event.objects.filter(pk=int(request.data['event'])).first()
+        publication_type = PublicationType.objects.get(
+            name=request.data['publication_type'])
+
         publication = Publication.objects.create(
             file=file,
-            event=event
+            event=event,
+            order=int(request.data.get('order')),
+            publication_type=publication_type
         )
         publication.generate_name()
         publication.file.save(publication.name, file)
