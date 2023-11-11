a841b2d702d1dff6f1331b7f94dd3a5edc505cfd
kovacspe <pet.kovacs1@gmail.com>
2023-11-11 16:32:44 +0100

    format

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 
 app_name = 'user'
 
-urlpatterns = [path('', include('dj_rest_auth.urls')),
-               path('registration/', include('dj_rest_auth.registration.urls')),
-               ]
+urlpatterns = [
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
+]
