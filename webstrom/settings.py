97c58368c73f1c68ea08db26dfcea8d7fbb433a8
rtrembecky <rtrembecky@gmail.com>
2023-11-12 10:59:01 +0100

    remove explicit date format

--- webstrom/settings.py
+++ webstrom/settings.py
@@ -109,7 +109,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'DATETIME_FORMAT': "%Y-%m-%dT%H:%M:%S.%fZ",
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
