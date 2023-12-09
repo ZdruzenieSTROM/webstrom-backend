12e3b61eaba35dc34d771e5ddbba92e28242a6c3
kovacspe <pet.kovacs1@gmail.com>
2023-12-09 22:20:26 +0100

    lint

--- cms/views.py
+++ cms/views.py
@@ -2,7 +2,6 @@
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.request import Request
 from rest_framework.response import Response
 
