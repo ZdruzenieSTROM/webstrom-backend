202b1edcba9931ecc166dff4610f7c67697b5ed7
kovacspe <pet.kovacs1@gmail.com>
2024-04-12 21:54:07 +0200

    log

--- competition/models.py
+++ competition/models.py
@@ -1,4 +1,5 @@
 import datetime
+import sys
 from typing import Optional
 
 from django.conf import settings
@@ -192,8 +193,8 @@ class Event(models.Model):
     @classmethod
     def can_user_create(cls, user: User, data: dict) -> bool:
         competition = Competition.objects.get(pk=data['competition'])
-        print(competition.can_user_modify(user))
-        print(competition.permission_group.all())
+        print(competition.can_user_modify(user), file=sys.stderr)
+        print(competition.permission_group.all(), file=sys.stderr)
         return competition.can_user_modify(user)
 
     def can_user_participate(self, user):
