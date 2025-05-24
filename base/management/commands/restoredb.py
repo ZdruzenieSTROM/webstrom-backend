593cda3a27d98c3f6b1774e838dd7507f28c609d
Martin Mihálik <mihalik.martin@outlook.sk>
2025-05-24 12:19:13 +0200

    Fix issues with registration links when updating events

--- base/management/commands/restoredb.py
+++ base/management/commands/restoredb.py
@@ -27,26 +27,11 @@ class Command(BaseCommand):
             self.stdout.write('Do not run this command with DEBUG=False')
             return
 
-        for app in settings.LOCAL_APPS:
-            app_dir = os.path.join(settings.BASE_DIR, app.split('.')[0])
-
-            pycache_dirs = [os.path.join(app_dir, pycache_dir, '__pycache__')
-                            for pycache_dir in APP_PYCACHE_DIRS]
-            for pycache_dir in pycache_dirs:
-                remove_contents(pycache_dir)
-
-            migrations_dir = os.path.join(app_dir, 'migrations')
-            remove_contents(migrations_dir, exclude=['__init__.py'])
-
-        remove_contents(os.path.join(
-            settings.BASE_DIR, 'webstrom', '__pycache__'))
-
         database = settings.DATABASES['default'].get('NAME', '')
 
         if os.path.exists(database):
             os.remove(database)
 
-        call_command('makemigrations')
         call_command('migrate')
 
         load_fixture = partial(call_command, 'loaddata')
