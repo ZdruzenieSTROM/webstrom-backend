015e777d1042501faed55ecbdb520360393a0037
kovacspe <pet.kovacs1@gmail.com>
2025-06-07 15:04:33 +0200

    Revert "Pridana konverzia HTML na markdown"
    This reverts commit af6a25b865a559f973d10eec9faa9f9156f31817.

--- data_migration/delete_all.sql
+++ data_migration/delete_all.sql
@@ -5,10 +5,9 @@ DELETE FROM public.competition_eventregistration;
 DELETE FROM public.competition_publication;
 DELETE FROM public.competition_problem;
 DELETE FROM public.competition_series;
-DELETE FROM public.competition_registrationlink;
-DELETE FROM public.competition_semester_late_tags;
 DELETE FROM public.competition_semester;
 DELETE FROM public.competition_event;
+DELETE FROM public.competition_registrationlink;
 
 DELETE FROM public.cms_flatpage;
 DELETE FROM public.cms_post_sites; 
